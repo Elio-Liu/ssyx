82491803a5c69f52035b23678b802490e6a6172a
elio <elioliu@outlook.com>
2023-06-14 00:44:49 +0900

    Initial commit

---
+++ src/main/java/com/elio/ssyx/SsyxApplication.java
@@ -0,0 +1,13 @@
+package com.elio.ssyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsyxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsyxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elio/ssyx/SsyxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elio.ssyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsyxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
